b3d62327ddf2d8eb14cbbf8625bd11a969cbfe91
Jeremy Arr <jeremyarr@gmail.com>
2017-08-01 16:59:48 +1000

    testing uses virtuenv

--- marbl/bag/dummy.py
+++ marbl/bag/dummy.py
@@ -2,7 +2,9 @@ from ..base import Marbl
 import asyncio
 
 class Dummy(Marbl):
-    def __init__(self, raise_error=None, sleep_for=0.01, sleep_lightly_for=0):
+    def __init__(self, raise_error=None, 
+            sleep_for=0.01, sleep_lightly_for=0,
+            ):
         self.raise_error = raise_error
         self.sleep_for = sleep_for
         self.sleep_lightly_for = sleep_lightly_for
